8959fce41789c6f9401e27b5b7116bf760114bb5
Ayyub <ayyubm459@gmail.com>
2024-03-02 16:17:13 +0530

    Remove proj

--- proj/app1/forms.py
+++
@@ -1,9 +0,0 @@
-# your_app/forms.py
-from django import forms
-from .models import Complaint
-
-class ComplaintForm(forms.ModelForm):
-    class Meta:
-        model = Complaint
-        fields = ['profile_id', 'subject', 'current_handler', 'created_at', 'updated_at']
-        # Add or remove fields based on your model's structure
