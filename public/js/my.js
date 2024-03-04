0f75c7c73b2cb9b343a6968d88e289fe77446be1
Phanendra sahu <Phanendrasahu2001@gmail.com>
2024-03-04 16:46:06 +0530

    updated Navigation

---
+++ public/js/my.js
@@ -0,0 +1,14 @@
+
+function loadContent(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => console.error('Error fetching content:', error));
+}
+//search bar script
+
+
+
+        
